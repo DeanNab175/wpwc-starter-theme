8263e25dac6e02f88e1045b1943110f16cf8cbec
Satyajit NABEEBACUS <sn@wshop-cloudcommerce.com>
2020-09-23 08:35:53 +0400

    added theme support and bootstrap walker nav menu

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+
+/**
+ * Enqueue styles and scripts
+ */
 function wpwc_starter_enqueue() {
     wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
 
@@ -16,3 +21,64 @@ function wpwc_starter_enqueue() {
 }
 
 add_action('wp_enqueue_scripts', 'wpwc_starter_enqueue');
+
+
+/**
+ * Add theme support for
+ * 1. Title tag
+ * 2. Post thumbnails
+ */
+add_theme_support('title-tag');
+add_theme_support('post-thumbnails');
+
+
+/**
+ * Register Nav menus
+ */
+register_nav_menus(array(
+    'header' => 'Custom Primary Menu'
+));
+
+
+/**
+ * Register widgets
+ */
+function wpwc_starter_widgets_init() {
+    register_sidebar(array(
+        'name'  => 'Footer 1',
+        'id'    => 'footer_1',
+        'before_widget' => '<div id="%1$s" class="widget%2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4 class="ttl">',
+        'after_title'   => '</h4>'
+    ));
+
+    register_sidebar( array(
+        'name'          => 'Footer 2',
+        'id'            => 'footer_2',
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4 class="ttl">',
+        'after_title'   => '</h4>'
+    ) );
+
+    register_sidebar( array(
+        'name'          => 'Footer 3',
+        'id'            => 'footer_3',
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4 class="ttl">',
+        'after_title'   => '</h4>'
+    ) );
+
+    register_sidebar( array(
+        'name'          => 'Sidebar',
+        'id'            => 'sidebar',
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4 class="ttl">',
+        'after_title'   => '</h4>'
+    ) );
+}
+
+add_action('widgets_init', 'wpwc_starter_widgets_init');
